9a994ab90306e403de03f5a6197454ba921ac087
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-03-04 18:01:45 +0300

    creditcart Algorithm  class added

--- src/main/java/com/mahdidroid/strategypattern/StrategypatternApplication.java
+++ src/main/java/com/mahdidroid/strategypattern/StrategypatternApplication.java
@@ -1,5 +1,9 @@
 package com.mahdidroid.strategypattern;
 
+import com.mahdidroid.strategypattern.model.CreditCardAlgorithm;
+import com.mahdidroid.strategypattern.model.PaypalAlgorithm;
+import com.mahdidroid.strategypattern.model.Product;
+import com.mahdidroid.strategypattern.model.ShoppingCart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +12,18 @@ public class StrategypatternApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StrategypatternApplication.class, args);
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.addProduct(new Product("123",11));
+        shoppingCart.addProduct(new Product("1789",55));
+        shoppingCart.addProduct(new Product("889",44));
+
+        shoppingCart.pay(new CreditCardAlgorithm("pop","60379914"));
+        shoppingCart.pay(new PaypalAlgorithm("mehdi@gmail.com","6678"));
+
+
+
+
+
     }
 
 }
